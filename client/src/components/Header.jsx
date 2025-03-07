5c29c77f34cc4aeb3aef384dc7a9549c29c07b1e
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-08 01:21:25 +0200

    updating the styling the header to postion fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 export default function Header(){
     
     return (
-      <header className="absolute inset-x-0 top-0 z-50 bg-white">
+      <header className="fixed inset-x-0 top-0 z-50 bg-white shadow-md">
           <Navigation/>
       </header>
     )
