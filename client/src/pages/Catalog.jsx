3e7790610704850c82efa5bd78f7e6641fca2408
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-09 00:24:03 +0200

    updating the posts page name with catalog

---
+++ client/src/pages/Catalog.jsx
@@ -0,0 +1,8 @@
+export default function Catalog(){
+    return(
+        
+    <div className="relative min-h-screen flex items-center justify-center text-center bg-cover bg-center">
+        <h1>Catalog page</h1>
+    </div>
+    )
+}
