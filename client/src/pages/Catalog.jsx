3b45de411d4295b936f817cdcc66a45055c15c62
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-09 22:06:02 +0200

    updated the catalog page to show all the available posts in the server and a create post button

--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -1,8 +1,37 @@
-export default function Catalog(){
-    return(
-        
-    <div className="relative min-h-screen flex items-center justify-center text-center bg-cover bg-center">
-        <h1>Catalog page</h1>
+import { useEffect, useState } from 'react';
+import catalogServices from '../services/catalogServices';
+import PostCard from '../components/PostCard';
+import CreatePost from '../components/CreatePost';
+
+export default function Catalog() {
+  const [posts, setPosts] = useState([]);
+  const [createFormActive, setCreateFormActive] = useState(false);
+
+  useEffect(() => {
+    catalogServices.getAll()
+      .then(response => setPosts(Object.values(response)));
+  }, []);
+
+  return (
+    <div className="relative min-h-screen bg-gray-100 pt-20">
+      <div className="flex justify-start pt-10 m-10">
+        <button 
+        onClick={() => setCreateFormActive(state => !state)}
+        className="bg-blue-500 text-white py-2 px-6 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
+        >
+          Create Post
+        </button>
+      </div>
+
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {posts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
+      </div>
+
+      {createFormActive && <CreatePost/>}
     </div>
-    )
+  );
 }
