f03e023b558a6e810975ee718125f33afd9ac7a9
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-15 00:51:43 +0200

    updating the name value of each input to match the server and set a submitbutton again because formAction usage to use the pending state of the button

--- client/src/components/CreatePost/CreatePost.jsx
+++ client/src/components/CreatePost/CreatePost.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import catalogServices from "../../services/catalogServices";
+import { useNavigate } from "react-router";
+import SubmitButton from "../SubmitButton/SubmitButton";
 
 const sportTypes = [
     "Football",
@@ -18,12 +20,14 @@ const sportTypes = [
 
 const CreatePost = () => {
     const {user} = useAuth();
+    const nav = useNavigate();
     const [error, setError] = useState("");
 
-    const formSubmit = async (formData) => {
+    const formAction = async (formData) => {
         const errors = [];
-        const image = formData.get("postImg");
-        const sportType = formData.get("postSport");
+        const image = formData.get("imageUrl");
+        const sportType = formData.get("sportType");
+        
 
         if(!/^(https?:\/\/).*\.(jpg|png|jgeg)$/i.test(image)){
             errors.push("Invalid Image URL");
@@ -33,8 +37,8 @@ const CreatePost = () => {
             errors.push("Please choose a sport type");
         }
 
-        if(errors){
-            setError(errors.join(" "))
+        if(errors.length > 0){
+                setError(errors.join(" "))
             setTimeout(() => {
                 setError("")
             }, 2000)
@@ -42,8 +46,8 @@ const CreatePost = () => {
         }
 
         const data = Object.fromEntries(formData);
-        console.log(data)
-        await catalogServices.createPost(data, user);
+        await catalogServices.create(data, user);
+        nav("/catalog")
 
     }
 
@@ -54,17 +58,17 @@ const CreatePost = () => {
                     <h1 className="text-3xl font-bold text-center mb-4">Create Post</h1>
 
                     {error && <p className="text-red-500 text-center mb-4">{error}</p> }
-                    <form id="create" action={formSubmit} className="space-y-4">
+                    <form className="space-y-4" action={formAction}>
                         <div>
                             <label 
-                            htmlFor="postTitle"
+                            htmlFor="title"
                             className="block text-2xl text-gray-700 font-medium mb-3"
                             >
                                 Title
                             </label>
                             <input 
                             type="text" 
-                            name="postTitle" 
+                            name="title" 
                             placeholder="Enter the title of your post"
                             className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                             required
@@ -73,14 +77,14 @@ const CreatePost = () => {
 
                         <div>
                             <label 
-                            htmlFor="postImg"
+                            htmlFor="imageUrl"
                             className="block text-2xl text-gray-700 font-medium mb-2"
                             >
                                 Image
                             </label>
                             <input 
                             type="text" 
-                            name="postImg" 
+                            name="imageUrl" 
                             placeholder="Paste an image URL..."
                             className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                             required
@@ -88,13 +92,13 @@ const CreatePost = () => {
                         </div>
                         <div>
                             <label 
-                            htmlFor="postDesc"
+                            htmlFor="description"
                             className="block text-2xl text-gray-700 font-medium mb-2"
                             >
                                 Description
                             </label>
                             <textarea
-                            name="postDesc"
+                            name="description"
                             placeholder="A brief description of the post"
                             className="w-full min-h-[140px] resize-none px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                             >
@@ -102,13 +106,13 @@ const CreatePost = () => {
                         </div>
                         <div>
                             <label 
-                            htmlFor="postSport"
+                            htmlFor="sportType"
                             className="block text-2xl text-gray-700 font-medium mb-2"
                             >
                                 Sport Type
                             </label>
 
-                            <select name="postSport" className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-md">
+                            <select name="sportType" className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-md">
                                 <option key="none" value="none">------</option>
                                 {sportTypes.map(sport => (
                                     <option key={sport} value={sport}>{sport}</option>
@@ -116,14 +120,8 @@ const CreatePost = () => {
                             </select>
                         </div>
 
-                        <div>
-                        <button
-                            type="submit"
-                            //disabled={pending}
-                            className="px-10 py-2 text-xl text-white bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-700 disabled:bg-gray-400 disabled:cursor-not-allowed"
-                            >
-                            Create
-                        </button>
+                        <div className="flex justify-center pt-4">
+                                <SubmitButton btnText="Create" />
                         </div>
                     </form>
                 </div>
--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -1,22 +1,23 @@
+import postDateConvertAgo from '../../utils/postDate';
 import styles from './PostCard.module.css';
 
 const PostCard = ({post}) => {
   return (
     <div className={styles.postCard}>
-        <img src={post.image_url} alt={`post-${post.title}`} className={styles.postImg} />
+        <img src={post.imageUrl} alt={`post-${post.title}`} className={styles.postImg} />
         <div className={styles.postContent}>
         <h3 className={styles.postTitle}>{post.title}</h3>
         <div className={styles.btnsContainer}>
             <div className={styles.actionsContainer}>
               <div className={styles.likeContainer}>
-                  <i className="fa-solid fa-heart"></i> {post.likes_count}
+                  <i className="fa-solid fa-heart"></i> {post.likesCount}
               </div>
               <div className={styles.commentContainer}>
-                  <i className="fa-regular fa-comment"></i> {post.comments_count}
+                  <i className="fa-regular fa-comment"></i> {post.commentsCount}
               </div>
             </div>
             <div>
-              <p className={styles.postDate}>{post.created_at.substring(0, 16).replace("T", " ")}</p>
+              <p className={styles.postDate}>{postDateConvertAgo(post.createdAt)}</p>
             </div>
         </div>
         </div>
