1cbb5f196b2dbb3e0a9d8ad67a04c3f9cf1589d0
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-15 23:36:12 +0200

    removed the responsive tailwind layout for the mobile and made sure it shows the protected routes if user if not the unprotected are visible

--- client/src/components/Header/Navigation.jsx
+++ client/src/components/Header/Navigation.jsx
@@ -1,19 +1,8 @@
-import { useState } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router';
 import { useAuth } from '../../context/AuthContext';
 
-const navigation = [
-  { name: 'Home', path: '/' },
-  { name: 'Athletes', path: '/athletes' },
-  { name: 'Catalog', path: '/catalog' },
-  { name: 'Create Post', path: '/catalog/create' },
-  { name: 'About', path: '/about' },
-]
 
 export default function Navigation(){
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     const { isAuthenticated } = useAuth();
     
     return(
@@ -34,22 +23,16 @@ export default function Navigation(){
           </span>       
           </h1>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
-        </div>
+
         <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <Link key={item.name} to={item.path} className="text-sm/6 font-semibold text-gray-900">
-              {item.name}
-            </Link>
-          ))}
+
+          <Link key="home" to="/" className="text-sm/6 font-semibold text-gray-900">Home</Link>
+          <Link key="athletes" to="/athletes" className="text-sm/6 font-semibold text-gray-900">Athletes</Link>
+          <Link key="catalog" to="/catalog" className="text-sm/6 font-semibold text-gray-900">Catalog</Link>
+          {isAuthenticated &&
+          <Link key="catalog-create" to="/catalog/create" className="text-sm/6 font-semibold text-gray-900">Create Post</Link>
+          }
+          <Link key="about" to="/about" className="text-sm/6 font-semibold text-gray-900">About</Link>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
 
@@ -64,54 +47,7 @@ export default function Navigation(){
         }
         </div>
       </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link to="/" className="-m-1.5 p-1.5">
-              <img
-                alt="logo-img"
-                src="/images/logo-sport.png"
-                className="h-15 w-auto"
-              />
-            </Link>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link
-                    to={item.path}
-                    key={item.name}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-              <div className="py-6">
-              {isAuthenticated ?
-                <Link to="/profile" className="p-3 border-4 rounded-full border-blue-500 cursor-pointer hover:bg-blue-500 hover:text-white transition-all duration-300 ease-in-out">
-                  <i className="fa-solid fa-user"></i> 
-                </Link>
-                :
-                <Link to="/login" className="text-sm/6 font-semibold text-gray-900">
-                Log in <span aria-hidden="true">&rarr;</span>
-                </Link>  
-              }
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
+
         </>
     )
 }
