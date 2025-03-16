ec005421378081b113df482db45f38a2cd750e65
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-16 03:16:56 +0200

    removed the create post btn to a seperate page

--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react';
 import catalogServices from '../services/catalogServices';
-import PostCard from '../components/PostCard';
-import CreatePost from '../components/CreatePost';
+import PostCard from '../components/PostCard/PostCard';
 
 export default function Catalog() {
   const [posts, setPosts] = useState([]);
-  const [createFormActive, setCreateFormActive] = useState(false);
 
   useEffect(() => {
     catalogServices.getAll()
@@ -13,15 +11,7 @@ export default function Catalog() {
   }, []);
 
   return (
-    <div className="relative min-h-screen bg-gray-100 pt-20">
-      <div className="flex justify-start pt-10 m-10">
-        <button 
-        onClick={() => setCreateFormActive(state => !state)}
-        className="bg-blue-500 text-white py-2 px-6 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
-        >
-          Create Post
-        </button>
-      </div>
+    <div className="relative min-h-screen bg-gray-100 py-50">
 
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
@@ -31,7 +21,6 @@ export default function Catalog() {
         </div>
       </div>
 
-      {createFormActive && <CreatePost/>}
     </div>
   );
 }
