931096077088cb37d2464c83de523e7aa2117da6
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-19 02:58:03 +0200

    udpated the requester to handle empty response when logout for ex and set the options to default to be able to change them in authenticated requests by passing the token

--- client/src/utils/requester.js
+++ client/src/utils/requester.js
@@ -1,23 +1,23 @@
-export const requester = async (method, url, data) => {
-    let options = {};
-
+const requester = async (method, url, data, options={}) => {
     if(method != "GET"){
-        options = {
-            method
-        }
+        options.method = method
     }
 
     if(data) {
         options = {
             ...options,
             headers : {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                ...options.headers
             },
             body: JSON.stringify(data)
         }
     }
 
     const response = await fetch(url, options);
+    if(!response.headers.get("Content-Type")){
+        return
+    }
     const result = await response.json();
 
     return result
@@ -29,4 +29,5 @@ export default {
     put: requester.bind(null, "PUT"),
     post: requester.bind(null, "POST"),
     delete: requester.bind(null, "DELETE"),
+    baseRequest: requester
 }
