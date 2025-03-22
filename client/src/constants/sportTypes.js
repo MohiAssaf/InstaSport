4e919666cc323bbe0caf9d63a2ff6267d103eab8
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-22 21:44:38 +0200

    addiing the sport types list in a seperate file to use in both the create and edit forms

---
+++ client/src/constants/sportTypes.js
@@ -0,0 +1,12 @@
+export const sportTypes = [
+    "Football",
+    "Basketball",
+    "Cricket",
+    "Tennis",
+    "American Football",
+    "Baseball",
+    "Golf",
+    "Mixed Martial Arts (MMA)",
+    "Ice Hockey",
+    "Other"
+];
