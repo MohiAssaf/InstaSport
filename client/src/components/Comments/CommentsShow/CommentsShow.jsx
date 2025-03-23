adf9f50c7b2667ac70c295d9d3c7651e78a1c4cb
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-24 00:28:07 +0200

    adding edit and delete buttons to the comment section storing the comment edited clicked in state and also making the edit and delete buttons visible for the author of the comment and only the owner can see the delete btn only

--- client/src/components/Comments/CommentsShow/CommentsShow.jsx
+++ client/src/components/Comments/CommentsShow/CommentsShow.jsx
@@ -1,13 +1,66 @@
+import { useState } from 'react';
 import styles from './CommentsShow.module.css'
 
-const CommentsShow = ({postComments}) => {
+const CommentsShow = ({postComments, postOwner}) => {
+    const [commentEdit, setCommentEdit] = useState({});
 
+    const handleEditCommentClick = (comment) => {
+        setCommentEdit({commentId: comment._id, commentContent: comment.content})
+    }
+
+    const handleEditCommentSubmit = () => {
+        console.log(`comment edited: ${commentEdit.commentId}`)
+    }
+
+    
     return (
             <div className={styles.container}>
                 {postComments.map(comment => (
                     <div key={comment._id} className={styles.comment} style={comment?.pending ? {opacity: 0.4}: {}}>
-                        <img src={comment?.author.profileImg} alt={`user-${comment?.author._id}`} />
-                        <p>{comment?.author.username}: {comment.content}</p>
+                        <div className={styles.commentConetent}>
+                            <img src={comment?.author.profileImg} alt={`user-${comment?.author._id}`} />
+                            {commentEdit?.commentId === comment._id ? (
+                                <input 
+                                type="text" 
+                                placeholder='Edit comment'
+                                defaultValue={commentEdit.commentContent}
+                                className={styles.editCommentInput}
+                                
+                                />
+                            ): (<p>{comment?.author.username}: {comment.content}</p>)}
+
+                        </div>
+
+                        {(comment?.author || postOwner) &&
+                        <div className={styles.commentActions}>
+                            {commentEdit?.commentId === comment._id ? (
+                                <>
+                                    <button
+                                    className={styles.commentUpdateBtns}
+                                    onClick={handleEditCommentSubmit}
+                                    >Update
+                                    </button>
+                                    <button
+                                    className={styles.commentUpdateBtns}
+                                    onClick={() => setCommentEdit({})}
+                                    >Cancel
+                                    </button>
+                                </>
+                            ): (
+                                <>
+                                    {comment?.author &&  (
+                                        <i 
+                                        className="text-blue-600 fa-solid fa-pen-to-square"
+                                        onClick={() => handleEditCommentClick(comment)}
+                                        ></i>
+                                    )}
+                                    <i 
+                                    className="text-red-700 fa-solid fa-circle-xmark"
+                                    ></i>
+                                </>
+                            )}
+                        </div>
+                        }
                     </div>
                 ))}
             </div>
