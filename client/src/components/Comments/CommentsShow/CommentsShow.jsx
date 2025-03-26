9fe521deccdd8e31c752c7d0865fea71b4eec6b2
MohiAssaf <mohamedabuassaf@gmail.com>
2025-03-27 01:45:40 +0200

    adding the comment edit in the comments show functionality to have more styled way on edit to update the edit content container with with the edit form and showing the buttons depeding if author of the comment or postowner

--- client/src/components/Comments/CommentsShow/CommentsShow.jsx
+++ client/src/components/Comments/CommentsShow/CommentsShow.jsx
@@ -1,68 +1,72 @@
-import { useState } from 'react';
 import styles from './CommentsShow.module.css'
+import CommentEdit from '../CommentEdit/CommentEdit';
+import { useCallback, useState } from 'react';
+import { useEditComment } from '../../../api/commentApi';
 
-const CommentsShow = ({postComments, postOwner}) => {
-    const [commentEdit, setCommentEdit] = useState({});
+const CommentsShow = ({ postComments, postOwner, onEditComment, currentUser}) => {
+    const {edit} = useEditComment();
+    const [commentToEdit, setCommentToEdit] = useState(null);
+    
+    const editCommentAction = useCallback(async (formData) => {
+        const updatedComment = formData.get("updatedComment");
 
-    const handleEditCommentClick = (comment) => {
-        setCommentEdit({commentId: comment._id, commentContent: comment.content})
-    }
+        if(updatedComment === commentToEdit.content || updatedComment === ''){
+            setCommentToEdit(null);
+            return
+        }
+        
 
-    const handleEditCommentSubmit = () => {
-        console.log(`comment edited: ${commentEdit.commentId}`)
-    }
+        const editedComment = {...commentToEdit, content: updatedComment};
 
+        await edit(editedComment._id, editedComment);
+        onEditComment(commentToEdit._id, editedComment)
+        setCommentToEdit(null);
+    }, [commentToEdit, onEditComment])
     
     return (
             <div className={styles.container}>
-                {postComments.map(comment => (
-                    <div key={comment._id} className={styles.comment} style={comment?.pending ? {opacity: 0.4}: {}}>
-                        <div className={styles.commentConetent}>
-                            <img src={comment?.author.profileImg} alt={`user-${comment?.author._id}`} />
-                            {commentEdit?.commentId === comment._id ? (
-                                <input 
-                                type="text" 
-                                placeholder='Edit comment'
-                                defaultValue={commentEdit.commentContent}
-                                className={styles.editCommentInput}
-                                
-                                />
-                            ): (<p>{comment?.author.username}: {comment.content}</p>)}
+                {postComments.map(comment => {
+                    const isCommentEditClicked= commentToEdit?._id === comment._id;
+                    const isCommentAuthorUser = comment?.author._id ===currentUser._id
+                    console.log(currentUser)
+                    
+                    return (
+                        <div key={comment._id} className={styles.comment} style={comment?.pending ? {opacity: 0.4}: {}}>
 
-                        </div>
+                            <div className={`${styles.commentConetent} ${isCommentEditClicked  ? styles.editActive: ''}`}>
+                                <img src={comment?.author.profileImg} alt={`user-${comment?.author._id}`} />
+                                {isCommentEditClicked? (
+                                    <CommentEdit 
+                                    commentData={commentToEdit}
+                                    onSubmitForm={editCommentAction}
+                                    onCancel={() => setCommentToEdit(null)}
+                                    />
+                                ): (<p>{comment?.author.username}: {comment.content}</p>)}
 
-                        {(comment?.author || postOwner) &&
-                        <div className={styles.commentActions}>
-                            {commentEdit?.commentId === comment._id ? (
+                            </div>
+
+                            {((isCommentAuthorUser|| postOwner) && !isCommentEditClicked) &&
+                            <div className={styles.commentActions}>
                                 <>
-                                    <button
-                                    className={styles.commentUpdateBtns}
-                                    onClick={handleEditCommentSubmit}
-                                    >Update
-                                    </button>
-                                    <button
-                                    className={styles.commentUpdateBtns}
-                                    onClick={() => setCommentEdit({})}
-                                    >Cancel
-                                    </button>
-                                </>
-                            ): (
-                                <>
-                                    {comment?.author &&  (
+                                    {isCommentAuthorUser && (
                                         <i 
                                         className="text-blue-600 fa-solid fa-pen-to-square"
-                                        onClick={() => handleEditCommentClick(comment)}
+                                        onClick={() => setCommentToEdit(comment)}
                                         ></i>
+
                                     )}
+
                                     <i 
                                     className="text-red-700 fa-solid fa-circle-xmark"
                                     ></i>
+
                                 </>
-                            )}
+                            
+                            </div>
+                            }
                         </div>
-                        }
-                    </div>
-                ))}
+                    )
+                })}
             </div>
     );
 };
